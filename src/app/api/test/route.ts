37c0b2be8ecf65458c823990f1778210a396c938
kharle14 <enriquezkharl14@gmail.com>
2024-01-14 17:10:43 +0800

    replace yup to zod validation

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,8 +1,24 @@
 import { NextResponse } from 'next/server';
 
+import { CreateUserSchema } from '@/schemas/user';
 import connectDB from '@/utils/connectDB';
 
-export async function GET() {
+export async function POST(req: Request) {
   await connectDB();
-  return NextResponse.json({ message: 'Test connection for DB success' });
+  const body = await req.json();
+
+  const result = CreateUserSchema.safeParse(body);
+
+  if (!result.success) {
+    let errorMessage = '';
+    result.error.issues.forEach((issue) => {
+      errorMessage = `${errorMessage + issue.path[0]}: ${issue.message}. `;
+    });
+
+    return NextResponse.json({ error: errorMessage }, { status: 400 });
+  }
+
+  return NextResponse.json({
+    message: 'Test connection for DB success',
+  });
 }
