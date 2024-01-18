05fa412d7a197810b53a50bcfad8c826de460f91
kharle14 <enriquezkharl14@gmail.com>
2024-01-18 21:18:44 +0800

    get recipe route

---
+++ src/app/api/recipes/[id]/route.ts
@@ -0,0 +1,24 @@
+import { isValidObjectId } from 'mongoose';
+
+import Recipe from '@/models/recipe';
+import connectDB from '@/utils/connectDB';
+
+interface Params {
+  params: {
+    id: string;
+  };
+}
+
+export async function GET(_req: Request, { params }: Params) {
+  await connectDB();
+
+  if (!isValidObjectId(params.id))
+    return Response.json({ error: 'Invalid recipe Id!' }, { status: 422 });
+
+  const recipe = await Recipe.findById(params.id);
+
+  if (!recipe)
+    return Response.json({ error: 'Could not find recipe!' }, { status: 404 });
+
+  return Response.json({ recipe });
+}
